dbe61c4ce8a890200b1f2c05c31c85f48dc47bea
Ricky <rdean00@me.com>
2021-03-20 12:40:55 -0400

    basic setup

---
+++ junit/edu/ycp/cs320/tbagproj/controller/MultiplyNumbersControllerTest.java
@@ -0,0 +1,33 @@
+package edu.ycp.cs320.tbagproj.controller;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/*import edu.ycp.cs320.lab02.controller.MultiplyNumbersController;
+import edu.ycp.cs320.lab02.model.Numbers;
+
+public class MultiplyNumbersControllerTest {
+	private Numbers model;
+	private MultiplyNumbersController controller;
+	
+	@Before
+	public void setUp() {
+		model = new Numbers();
+		controller = new MultiplyNumbersController();
+		
+		model.setFirst(1.0);
+		model.setSecond(2.0);
+		model.setThird(3.0);
+		
+		controller.setModel(model);
+	}
+	
+	@Test
+	public void testMultiply() {
+		Double result = controller.multiply();
+		assertTrue(result == 6.0);
+	}
+}
+*/
---
+++ junit/edu/ycp/cs320/tbagproj/model/GuessingGameTest.java
@@ -0,0 +1,59 @@
+package edu.ycp.cs320.tbagproj.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.ycp.cs320.tbagproj.model.GuessingGame;
+
+public class GuessingGameTest {
+	private GuessingGame model;
+	
+	@Before
+	public void setUp() {
+		model = new GuessingGame();
+	}
+	
+	@Test
+	public void testSetMin() {
+		model.setMin(1);
+		assertEquals(1, model.getMin());
+	}
+	
+	public void testSetMax() {
+		model.setMax(100);
+		assertEquals(100, model.getMax());
+	}
+	
+	@Test
+	public void testIsDone() {
+		model.setMax(4);
+		model.setMin(4);
+		assertEquals(true, model.isDone());
+	}
+	
+	@Test
+	public void testGetGuess() {
+		model.setMin(1);
+		model.setMax(100);
+		assertEquals(50, model.getGuess());
+	}
+	
+	@Test
+	public void testSetIsLessThan() {
+		model.setMin(1);
+		model.setMax(100);
+		int guess = model.getGuess();
+		model.setIsLessThan(guess);
+		assertEquals(49, model.getMax());
+	}
+	
+	public void testSetIsGreaterThan() {
+		model.setMin(1);
+		model.setMax(100);
+		int guess = model.getGuess();
+		model.setIsGreaterThan(guess);
+		assertEquals(51, model.getMin());
+	}
+}
