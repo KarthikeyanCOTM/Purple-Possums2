d216678e0717f6d4f4f59db9660c4b8d1eae1d1f
Ricky <rdean00@me.com>
2021-04-03 16:47:50 -0400

    added creation of first three rooms

--- src/edu/ycp/cs320/tbagproj/model/Game.java
+++ src/edu/ycp/cs320/tbagproj/model/Game.java
@@ -15,7 +15,45 @@ public class Game {
 	public void newGame() {
 		HashMap<String, Room> Rooms = new HashMap<>();
 		Map map = new Map();
+		Room room = new Room();
+		Room tempRoom = new Room();
+		Item item = new Item();
+		Inventory tempInventory = new Inventory();
+		
+		//foyar creation
 		map.createRoom(null, "Foyar", null, Rooms);
+		room = map.findRoom("Foyar");
+		room.setDescription("A large room that has two pillars that reach up to the ceiling. There are some paintings on the walls with a door to the north, and one two the west.");
+		
+		//closet creation
+		Rooms.put("east", room);
+		map.createRoom(null, "Closet", null, Rooms);
+		tempRoom = map.findRoom("Closet");
+		tempRoom.setDescription("A walk in closet that has various coats, boots, and scarves along with a large open trunk. There is a door to the east");
+		tempInventory = tempRoom.getInventory();
+		item.setName("Fur Armor");
+		item.setArmour(1.0);
+		tempInventory.addItem(item);
+		item.setName("Wooden Sword");
+		item.setArmour(0);
+		item.setDamage(3.0);
+		tempInventory.addItem(item);
+		item.setName("Wooden Staff");
+		tempInventory.addItem(item);
+		tempRoom.setInventory(tempInventory);
+		room.setConnections("west", tempRoom);
+		
+		//main hall creation
+		Rooms.clear();
+		Rooms.put("south", room);
+		map.createRoom(null, "Main Hall", null, Rooms);
+		tempRoom = map.findRoom("Main Hall");
+		tempRoom.setDescription("A grand corridor filled with painting, sculptures, and tapastries. It has a door to the north, south, east, and west.");
+		tempInventory = tempRoom.getInventory();
+		tempInventory.addGold(5);
+		tempRoom.setInventory(tempInventory);
+		room.setConnections("north", tempRoom);
+		room = tempRoom;
 	}
 	
 	public void saveGame() {
