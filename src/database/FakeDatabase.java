bf43a13af92d27beb3908b7d92e0035909a3bffa
William Batchelor <wbatchelor@ycp.edu>
2021-04-19 20:55:37 -0400

    Derby changes

---
+++ src/database/FakeDatabase.java
@@ -0,0 +1,24 @@
+package database;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import edu.ycp.cs320.tbagproj.model.*;
+
+public class FakeDatabase implements IDatabase {
+
+	
+	public String saveGame(String key, Player player, Map map) {
+		
+		return null;
+	}
+
+	
+	public String loadGame(String key) {
+		
+		return null;
+	}
+
+}
